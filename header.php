07ca04cafecd0c7d3b371a36024a40882d704312
sennecoolgames <sennevanwesemael@gmail.com>
2025-10-02 12:29:09 +0200

    Moved to dash.php

--- header.php
+++ header.php
@@ -10,8 +10,8 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <li class="nav-button <?php if ($currentPage == 'profile.php') echo 'active'; ?>">
             <a href="profile.php">Profile</a>
         </li>
-        <li class="nav-button <?php if ($currentPage == 'settings.php') echo 'active'; ?>">
-            <a href="settings.php">Settings</a>
+        <li class="nav-button <?php if ($currentPage == 'dash.php') echo 'active'; ?>">
+            <a href="dash.php">Dashboard</a>
         </li>
         <li class="nav-button <?php if ($currentPage == 'logout.php') echo 'active'; ?>">
             <a href="logout.php">Logout</a>
